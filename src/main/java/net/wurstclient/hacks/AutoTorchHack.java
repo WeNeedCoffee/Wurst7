67bb5e2fc28bae7b5c8f34c631f2f4ef035faae4
Daleth Darko <Daleth@Dalethium.com>
2020-01-25 20:22:50 -0800

    Work on auto torch

--- src/main/java/net/wurstclient/hacks/AutoTorchHack.java
+++ src/main/java/net/wurstclient/hacks/AutoTorchHack.java
@@ -1,5 +1,7 @@
 package net.wurstclient.hacks;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
@@ -32,42 +34,68 @@ public final class AutoTorchHack extends Hack implements UpdateListener {
 		EVENTS.remove(UpdateListener.class, this);
 	}
 
+	int e = 0;
+
 	@Override
 	public void onUpdate() {
-		if (!d)
-			checkAround((int) Math.round(WurstClient.MC.player.getPos().x), (int) Math.round(WurstClient.MC.player.getPos().y), (int) Math.round(WurstClient.MC.player.getPos().z));
 
-	}
-
-	static boolean d = false;
-	static int radius = 4;
-
-	public static void checkAround(int x, int y, int z) {
-		d = true;
-		for (int xx = -radius; xx <= radius; xx++) {
-			for (int yy = -radius; yy <= radius; yy++) {
-				for (int zz = -radius; zz <= radius; zz++) {
-					BlockPos pos = new BlockPos(xx + x, yy + y, zz + z);
-					if (MC.world.isChunkLoaded(pos) && MC.player.getPos().distanceTo(new Vec3d(pos)) < 7) {
-						if (BlockUtils.getState(pos).isAir() && MC.player.clientWorld.getLightLevel(net.minecraft.world.LightType.BLOCK, pos) < 8 && placeTorch(pos)) {
-							d = false;
+		int reach = 3;
+		if (!d) {
+			int x = WurstClient.MC.player.getBlockPos().getX();
+			int y = WurstClient.MC.player.getBlockPos().getY();
+			int z = WurstClient.MC.player.getBlockPos().getZ();
+			for (int xx = -reach; xx <= reach; xx++) {
+				for (int yy = -reach; yy <= reach; yy++) {
+					for (int zz = -reach; zz <= reach; zz++) {
+						BlockPos pos = new BlockPos(xx + x, yy + y, zz + z);
+						if (Math.round(MC.player.getPos().distanceTo(new Vec3d(pos))) > 6) {
+							tocheck.clear();
 							return;
 						}
-					} else {
-						d = false;
-						return;
+						tocheck.add(pos);
 					}
 				}
 			}
+			d = true;
+		} else {
+			if (!tocheck.isEmpty()) {
+				for (BlockPos pos : tocheck) {
+					if (MC.world.isChunkLoaded(pos) && Math.round(MC.player.getPos().distanceTo(new Vec3d(pos))) <= 6) {
+						if (BlockUtils.getState(pos).isAir() && MC.player.clientWorld.getLightLevel(net.minecraft.world.LightType.BLOCK, pos) < 8) {
+							toplace.add(pos);
+							break;
+						}
+					} else {
+						break;
+					}
+				}
+				tocheck.clear();
+			}
+			if (e > 1 && !toplace.isEmpty()) {
+				if (BlockUtils.getState(toplace.get(toplace.size() - 1)).isAir() && MC.player.clientWorld.getLightLevel(net.minecraft.world.LightType.BLOCK, toplace.get(toplace.size() - 1)) < 8) {
+					placeTorch(toplace.get(toplace.size() - 1));
+				}
+				d = false;
+				e = 0;
+				toplace.remove(toplace.size() - 1);
+			} else if (toplace.isEmpty()) {
+				d = false;
+				e = 0;
+			} else {
+				e++;
+			}
 		}
-		d = false;
 	}
+
+	List<BlockPos> tocheck = new ArrayList<>();
+	List<BlockPos> toplace = new ArrayList<>();
+	boolean d = false;
 
 	public static boolean placeTorch(BlockPos pos) {
 		Vec3d posVec = new Vec3d(pos).add(0.5, 0.5, 0.5);
 		Vec3d eyesPos = RotationUtils.getEyesPos();
 		IClientPlayerInteractionManager im = IMC.getInteractionManager();
-		double rangeSq = Math.pow(5, 2);
+		double rangeSq = Math.pow(6, 2);
 		for (Direction side : Direction.values()) {
 			BlockPos neighbor = pos.offset(side);
 
