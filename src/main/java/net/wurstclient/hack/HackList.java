1b25b0e4fda8ea48279a12fca2e9e0b41a5b0cfc
Daleth Darko <Daleth@Dalethium.com>
2020-01-28 18:18:28 -0800

    port of glide hack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -64,6 +64,7 @@ import net.wurstclient.hacks.FlightHack;
 import net.wurstclient.hacks.FollowHack;
 import net.wurstclient.hacks.FreecamHack;
 import net.wurstclient.hacks.FullbrightHack;
+import net.wurstclient.hacks.GlideHack;
 import net.wurstclient.hacks.HeadRollHack;
 import net.wurstclient.hacks.HighJumpHack;
 import net.wurstclient.hacks.ItemEspHack;
@@ -168,7 +169,7 @@ public final class HackList implements UpdateListener {
 	public final FreecamHack freecamHack = new FreecamHack();
 	public final FullbrightHack fullbrightHack = new FullbrightHack();
 	// public final GhostHandHack ghostHandHack = new GhostHandHack();
-	// public final GlideHack glideHack = new GlideHack();
+	public final GlideHack glideHack = new GlideHack();
 	public final HeadRollHack headRollHack = new HeadRollHack();
 	// public final HealthTagsHack healthTagsHack = new HealthTagsHack();
 	public final HighJumpHack highJumpHack = new HighJumpHack();
